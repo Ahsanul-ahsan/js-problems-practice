6f6dd27189591a8d9c622f26e70a834dce9778d2
Ahsanul-ahsan <ahsanul714@gmail.com>
2026-02-05 00:05:49 +0600

    task add

--- task.js
+++ task.js
@@ -10,7 +10,7 @@ function getmin(numbers){
     return min;
 }
 const min = getmin(heights1);
-console.log( 'Find the lowest number in the array :', min);
+// console.log( 'Find the lowest number in the array :', min);
 
 // Task-2 Find the friend with the smallest name
 const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
@@ -21,3 +21,23 @@ for (i = 1; i < heights2.length; i++){
         smallName = heights2[i];
 }
 // console.log( 'Find the friend with the smallest name: ', smallName);  
+
+
+// Task-3: Your task is to calculate the `total budget` required to buy electronics:
+// Write a JavaScript function named `calculateElectronicsBudget` that takes in the number of laptop, tablets, and mobile` and returns the `total money required
+
+const laptopPrice = 35000;
+const tabletPrice = 15000;
+const mobilePrice = 20000; 
+function calculateElectronicsBudget(laptop,tablet,mobile){
+    const laptopCost = laptop * laptopPrice;
+    const tabletCost = tablet * tabletPrice;
+    const mobileCost = mobile * mobilePrice;
+    const totalMoney = laptopCost + tabletCost + mobileCost;
+    return totalMoney;
+}
+const totalMoney = calculateElectronicsBudget(1,1,1);
+// console.log( 'total money required: ', totalMoney);
+
+
+
